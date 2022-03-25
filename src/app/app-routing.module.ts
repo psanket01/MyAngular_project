1969c7213449c5d74662979d18ea03a4b10ae2ed
psanket01 <psanket611@gmail.com>
2022-03-25 17:54:22 +0530

    first project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SendmsgComponent } from './pages/contact-us/sendmsg/sendmsg.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"contactUs", component:ContactUsComponent},
+  {path:"sendmsg", component:SendmsgComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SendmsgComponent } from './pages/contact-us/sendmsg/sendmsg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SendmsgComponent
+  
   ],
   imports: [
     BrowserModule,
